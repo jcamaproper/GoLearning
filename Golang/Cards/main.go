f7f92798d46b347f01a8abd47f667f45302c875e
jcamaproper <juan.camacho@proper.ai>
2022-01-25 14:43:03 -0600

    TIPOS

--- Golang/Cards/main.go
+++ Golang/Cards/main.go
@@ -2,25 +2,34 @@ package main
 
 import "fmt"
 
+// Create a new type of deck
+// which is a slice of string
+type deck []string
+
 func main() {
 
-	/* //card := "Ace of Spades"
-	card := newCard()
-
-	fmt.Println(card) */
-
-	cards := []string{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
-
-	for _, card := range cards {
+	//cards := []string{"Ace of Diamonds", newCard()}
+	//cards = append(cards, "Six of Spades")
+	cards := deck{"Ace of Diamonds", newCard()}
+	/* for _, card := range cards {
 
 		fmt.Println(card)
-	}
+	} */
 
+	cards.print()
 	fmt.Println(cards)
 }
 
 func newCard() string {
 
 	return "Five of Diamonds"
+}
+
+func (d deck) print() {
+
+	for i, card := range d {
+
+		fmt.Println(i, card)
+	}
+
 }
